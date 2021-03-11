7bff7f3ca24960ac93b00ee33f2cbbbd39dbf11b
davisantosr <davi-rocha2@hotmail.com>
2021-03-11 19:52:38 -0300

    express router

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
+import express from 'express';
+import router from './routes/auth.js';
 
 const app = express();
 
+app.use('/api', router)
 
-app.get('/', (req, res) => {
-  res.status(200).send('api is running')
-})
+
 
 app.listen(5000, () => console.log('API is running on port 5000'));
