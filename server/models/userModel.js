d3ffff8b84f76c4656dcbbb9ec672dda00dbad95
davisantosr <davi-rocha2@hotmail.com>
2021-03-13 22:18:58 -0300

    add bcrypt | hashing password | mongoose userSchema level

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from "bcryptjs";
 
 const {Schema} = mongoose;
 
@@ -29,5 +30,26 @@ const userSchema = new Schema({
     timestamps: true
   }
 )
+// 2th parameter need to be a conventional func not arrow
+userSchema.pre('save', async function(next) {
+  let user = this;
+  // hash the password only if user change or registering for the first time
+  // otherwise each time user.save() will update the password
+  
+  if(user.isModified('password')) {
+    const salt = await bcrypt.genSalt(12)
+    return bcrypt.hash(user.password, salt, function(err, hash) {
+      if(err) {
+        console.log('BCYPT HASH ERROR', err)
+        return next(err)
+      }
+      user.password = hash;
+      return next();
+    })
+  } else {
+    return next();
+  }
+  
+})
 
 export default mongoose.model('User', userSchema);
