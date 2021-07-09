7cf0a94fd8bc2427f8b860edd600c2bc5ff435c5
davisantosr <davi-rocha2@hotmail.com>
2021-07-09 19:49:56 -0300

    add private route | add Dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import {Provider} from 'react-redux';
-import {ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import Home from './booking/Home';
-import Login from './auth/Login';
-import Register from './auth/Register';
-import TopNav from './components/TopNav';
+import TopNav from "./components/TopNav";
+import Home from "./booking/Home";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import Dashboard from "./user/Dashboard";
 
-import {store} from './redux/store'
-
-
+import { store } from "./redux/store";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <div>
       <Provider store={store}>
         <BrowserRouter>
-          <TopNav/>
-          <ToastContainer position={'top-center'}/>
+          <TopNav />
+          <ToastContainer position={"top-center"} />
           <Switch>
-            <Route component={Home} exact path={'/'}/>
-            <Route component={Login} path={'/login'}/>
-            <Route component={Register} path={'/register'}/>
+            <Route component={Home} exact path={"/"} />
+            <Route component={Login} path={"/login"} />
+            <Route component={Register} path={"/register"} />
+            <PrivateRoute component={Dashboard} path={"/dashboard"} />
           </Switch>
         </BrowserRouter>
       </Provider>
--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -1,68 +1,64 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import LoginForm from '../components/LoginForm';
-import {useDispatch} from 'react-redux';
+import React, { useState } from "react";
+import axios from "axios";
+import LoginForm from "../components/LoginForm";
+import { useDispatch } from "react-redux";
 
-import { LOGGED_IN_USER } from '../redux/constants/auth';
+import { LOGGED_IN_USER } from "../redux/constants/auth";
 
-const Login = ({history}) => {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const Login = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const dispatch = useDispatch();
 
-    
   const handleLogin = async (user) => {
-    const {email, password} = user;
-    
+    const { email, password } = user;
+
     try {
       const res = await axios.post(`${process.env.REACT_APP_API}/login/`, {
-        email, password
-      })
-      
-      return res.data
-    }
-    catch(err) {
-      console.log(err)
-    }
+        email,
+        password,
+      });
 
-  }
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      let res = await handleLogin({email, password})
+      let res = await handleLogin({ email, password });
 
-      if(res) {
-        window.localStorage.setItem('auth', JSON.stringify(res))
+      if (res) {
+        window.localStorage.setItem("auth", JSON.stringify(res));
+        console.log(res);
 
         // save to Redux
         dispatch({
           type: LOGGED_IN_USER,
-          payload: res
-        })
+          payload: res,
+        });
+        history.push("/dashboard");
       }
-      
-      history.push('/')
-
-    } catch(err) {
-      console.log(err)
+    } catch (err) {
+      console.log(err);
       // if(err.response.status === 400) {
       //   toast.error(err.response.data)
       // }
     }
-  }
+  };
 
   return (
-    <> 
-      <div className='container-fluid bg-secondary h1 p-5 text-center'>
+    <>
+      <div className="container-fluid bg-secondary h1 p-5 text-center">
         <h1>Login</h1>
       </div>
 
-      <div className='container'>
-        <div className='row'>
+      <div className="container">
+        <div className="row">
           <div className="col-md-6 offset-md-3">
             <LoginForm
               handleSubmit={handleSubmit}
@@ -75,7 +71,7 @@ const Login = ({history}) => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
