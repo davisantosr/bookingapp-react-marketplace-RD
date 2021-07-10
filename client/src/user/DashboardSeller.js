dc1ff5ff71b053ce0b8e58d302f48610251af4eb
davisantosr <davi-rocha2@hotmail.com>
2021-07-09 21:57:21 -0300

    add antd, moment | add ConnectNav-will have stripe info

--- client/src/user/DashboardSeller.js
+++ client/src/user/DashboardSeller.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 import DashboardNav from "../components/DashboardNav";
+import ConnectNav from "../components/ConnectNav";
 
 const DashboardSeller = () => {
   return (
     <>
       <div className="container-fluid bg-secondary p-5">
-        <h1>Dashboard S</h1>
+        <ConnectNav />
       </div>
       <div className="container-fluid p-4">
         <DashboardNav />
